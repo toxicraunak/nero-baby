89bf65a2aa9b9bece0428dfe1316e0a1f04e498b
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-22 04:30:23 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,28 +1,63 @@
 
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6195725562 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    #####
+
+    ANILIST_CLIENT = getenv("ANILIST_CLIENT", "8679")
+  
+    ANILIST_SECRET = getenv("ANILIST_SECRET", "NeCEq9A1hVnjsjZlTZyNvqK11krQ4HtSliaM7rTN")
+  
+    API_ID = getenv("API_ID", None)
+   
+    API_HASH = getenv("API_HASH",None)
+   
+    TOKEN = getenv("TOKEN", None)
+  
+    OWNER_ID = getenv("OWNER_ID", "6195725562") 
+
+    OWNER_USERNAME = ("OWNER_USERNAME", "loco_x_Pilot")
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
-    START_IMG = ""
-    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
-    # RECOMMENDED
-BOT_USERNAME = getenv("BOT_USERNAME" , "PoliceBot")
-    DATABASE_URL = getenv("DATABASE_URL", " postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc")
-    CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = ""
-    # Get your API key from https://timezonedb.com/api
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "the_friendz")
+   
+    START_IMG = getenv("START_IMG", "https://graph.org/file/eaa3a2602e43844a488a5.jpg")
 
+    JOIN_LOGGER = getenv("JOIN_LOGGER", "-1002080453210")
+   
+    EVENT_LOGS = getenv("EVENT_LOGS",  "-1002080453210")
+  
+    ERROR_LOGS = getenv("ERROR_LOGS", "-1001929735324")
+
+    MONGO_DB_URI= getenv("MONGO_DB_URI", None)
+   
+    LOG_CHANNEL = getenv("LOG_CHANNEL", "-1001852000392")
+   
+    BOT_USERNAME = getenv("BOT_USERNAME" , "avishaBot")
+   
+    DATABASE_URL = getenv("DATABASE_URL", "postgres://bnastfeg:SSeKbnIRV5dkO_2ewW10Y0EOuvRVgo1f@kesavan.db.elephantsql.com/bnastfeg")
+
+    CASH_API_KEY = getenv("CASH_API_KEY", "V48U2FLLKRHSVD4X")
+    
+    TIME_API_KEY = getenv("TIME_API_KEY", "1CUBX1HXGNHW")
+
+    SPAMWATCH_API = getenv("SPAMWATCH_API", "3624487efd8e4ca9c949f1ab99654ad1e4de854f41a14afd00f3ca82d808dc8c")
+    
+    SPAMWATCH_SUPPORT_CHAT = getenv("SPAMWATCH_SUPPORT_CHAT", "h_cc_help")
+    
+    WALL_API = getenv("WALL_API", "2455acab48f3a935a8e703e54e26d121")
+    
+    REM_BG_API_KEY = getenv("REM_BG_API_KEY", "xYCR1ZyK3ZsofjH7Y6hPcyzC")
+    
+    OPENWEATHERMAP_ID = getenv("OPENWEATHERMAP_ID", "887da2c60d9f13fe78b0f9d0c5cbaade")
+
+    BAN_STICKER = getenv("BAN_STICKER", "CAACAgEAAxkBAAIrTWYljyX_lqcubkAzg0jy45CRvxAFAAKvAgACrLHoRU50VVvh3xWwNAQ")
+
+    HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+
+    HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+    
     # Optional fields
+    
     BL_CHATS = []  # List of groups that you want blacklisted.
     DRAGONS = []  # User id of sudo users
     DEV_USERS = []  # User id of dev users
@@ -47,3 +82,4 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
+    
